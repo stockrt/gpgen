40a8e1fdc14470b359f507e8b38b33c49d306323
Rogério Carvalho Schneider <stockrt@gmail.com>
2009-07-19 23:53:27 -0300

    Abs path for /misc

--- gpgen.py
+++ gpgen.py
@@ -44,7 +44,7 @@ extra_footer_links.append({'url': 'http://twitter.com/%s' % login,
                     'description': 'twitter'})
 extra_footer_links.append({'url': 'http://stockrtweb.homelinux.com',
                     'description': 'directory listing at home'})
-extra_footer_links.append({'url': 'misc/vitae/rogerio_schneider.pdf',
+extra_footer_links.append({'url': 'http://%s.github.com/misc/vitae/rogerio_schneider.pdf' % login,
                     'description': 'vitae'})
 extra_footer_links.append({'url': 'http://picasaweb.google.com/%s' % login,
                     'description': 'seeme'})
